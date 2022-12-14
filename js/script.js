40740778e682b26a202b547ff859e693df3c10a3
Matias Aramayo <matiasaramayo@hotmail.com>
2022-12-14 02:45:17 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const btn = document.getElementById('menu-btn')
 const nav = document.getElementById('menu')
+const cero = 0
+
 
 btn.addEventListener('click', ()=> {
     btn.classList.toggle('open');
